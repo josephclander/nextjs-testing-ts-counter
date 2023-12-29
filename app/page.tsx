3dc026d7e2508484d1538440bb9897fc680e6ba9
Joseph Lander <josephclander@gmail.com>
2023-12-29 10:52:13 +0000

    Add initial markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export const metadata = {
-  title: 'App Router',
-}
+  title: 'Counter App',
+};
 
 export default function Page() {
-  return <h1>App Router</h1>
+  return (
+    <>
+      <h1>Counter App</h1>
+      <div className="counter__container">
+        <div className="counter__display">1</div>
+        <div className="counter__buttons">
+          <button className="counter__increment">+</button>
+          <button className="counter__decrement">-</button>
+        </div>
+      </div>
+    </>
+  );
 }
